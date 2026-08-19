4df3ff7f574a9c41854b8498e205814f72d00031
Aluno Vocacao <mimvitoriasantossilva@gmail.com>
2026-08-19 11:37:58 -0300

    update

--- Modulo07/modulo07_yasmimvssilva_ativ1_utilidades.py
+++ Modulo07/modulo07_yasmimvssilva_ativ1_utilidades.py
@@ -8,3 +8,30 @@ def subtrair(a, b):
 
 def potencia(base, expoente):
     return base ** expoente
+
+def dividir(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a / b
+
+def divisao_inteira(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a // b
+
+def resto_divisao(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a % b
+
+def multiplicar(a, b):
+    return a * b
+
+def calcular_media(lista_numeros):
+
+    if not lista_numeros:
+        return 0
+    return sum(lista_numeros) / len(lista_numeros)
+
+def e_par(numero):
+    return numero % 2 == 0
